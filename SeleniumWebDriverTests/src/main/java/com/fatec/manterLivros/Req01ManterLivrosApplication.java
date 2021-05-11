090938499a15c30899900522be6990a10c54f588
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2021-05-11 20:56:27 -0300

    Atividade Teste de Software(até REQ05)

---
+++ SeleniumWebDriverTests/src/main/java/com/fatec/manterLivros/Req01ManterLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.manterLivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Req01ManterLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Req01ManterLivrosApplication.class, args);
+	}
+
+}
